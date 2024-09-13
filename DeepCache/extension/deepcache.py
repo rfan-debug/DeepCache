c170b71312ec68a4bfb6f0ebb3fa128ffe11a6fa
Ruixi Fan <ruixi@typeface.ai>
2024-09-13 16:27:33 -0700

    revert

--- DeepCache/extension/deepcache.py
+++ DeepCache/extension/deepcache.py
@@ -6,8 +6,8 @@ class DeepCacheSDHelper(object):
 
     def enable(self):
         if not self._enabled:
-            self._reset_states()
-            self._wrap_modules()
+            self.reset_states()
+            self.wrap_modules()
             self._enabled = True
             print("Enabling Deepcache")
         else:
@@ -15,8 +15,9 @@ class DeepCacheSDHelper(object):
 
     def disable(self):
         if self._enabled:
-            self._unwrap_modules()
-            self._reset_states()
+            self.unwrap_modules()
+            self.reset_states()
+            self._enabled = False
             print("Disabling Deepcache")
         else:
             print("DeepCache is already disabled.")
@@ -31,7 +32,7 @@ class DeepCacheSDHelper(object):
             'skip_mode': skip_mode
         }
 
-    def _is_skip_step(self, block_i, layer_i, blocktype ="down"):
+    def is_skip_step(self, block_i, layer_i, blocktype ="down"):
         self.start_timestep = self.cur_timestep if self.start_timestep is None else self.start_timestep # For some pipeline that the first timestep != 0
         cache_interval, cache_layer_id, cache_block_id, skip_mode = \
             self.params['cache_interval'], self.params['cache_layer_id'], self.params['cache_block_id'], self.params['skip_mode']
@@ -47,7 +48,7 @@ class DeepCacheSDHelper(object):
     def is_enter_position(self, block_i, layer_i):
         return block_i == self.params['cache_block_id'] and layer_i == self.params['cache_layer_id']
 
-    def _wrap_unet_forward(self):
+    def wrap_unet_forward(self):
         self.function_dict['unet_forward'] = self.pipe.unet.forward
         def wrapped_forward(*args, **kwargs):
             self.cur_timestep = list(self.pipe.scheduler.timesteps).index(args[1].item())
@@ -55,45 +56,45 @@ class DeepCacheSDHelper(object):
             return result
         self.pipe.unet.forward = wrapped_forward
 
-    def _wrap_block_forward(self, block, block_name, block_i, layer_i, blocktype ="down"):
+    def wrap_block_forward(self, block, block_name, block_i, layer_i, blocktype ="down"):
         self.function_dict[
             (blocktype, block_name, block_i, layer_i)
         ] = block.forward
         def wrapped_forward(*args, **kwargs):
-            skip = self._is_skip_step(block_i, layer_i, blocktype)
+            skip = self.is_skip_step(block_i, layer_i, blocktype)
             result = self.cached_output[(blocktype, block_name, block_i, layer_i)] if skip else self.function_dict[(blocktype, block_name,  block_i, layer_i)](*args, **kwargs)
             if not skip:
                 self.cached_output[(blocktype, block_name, block_i, layer_i)] = result
             return result
         block.forward = wrapped_forward
 
-    def _wrap_modules(self):
+    def wrap_modules(self):
         # 1. wrap unet forward
-        self._wrap_unet_forward()
+        self.wrap_unet_forward()
         # 2. wrap downblock forward
         for block_i, block in enumerate(self.pipe.unet.down_blocks):
             for (layer_i, attention) in enumerate(getattr(block, "attentions", [])):
-                self._wrap_block_forward(attention, "attentions", block_i, layer_i)
+                self.wrap_block_forward(attention, "attentions", block_i, layer_i)
             for (layer_i, resnet) in enumerate(getattr(block, "resnets", [])):
-                self._wrap_block_forward(resnet, "resnet", block_i, layer_i)
+                self.wrap_block_forward(resnet, "resnet", block_i, layer_i)
             for downsampler in getattr(block, "downsamplers", []) if block.downsamplers else []:
-                self._wrap_block_forward(downsampler, "downsampler", block_i, len(getattr(block, "resnets", [])))
-            self._wrap_block_forward(block, "block", block_i, 0, blocktype ="down")
+                self.wrap_block_forward(downsampler, "downsampler", block_i, len(getattr(block, "resnets", [])))
+            self.wrap_block_forward(block, "block", block_i, 0, blocktype ="down")
         # 3. wrap midblock forward
-        self._wrap_block_forward(self.pipe.unet.mid_block, "mid_block", 0, 0, blocktype ="mid")
+        self.wrap_block_forward(self.pipe.unet.mid_block, "mid_block", 0, 0, blocktype ="mid")
         # 4. wrap upblock forward
         block_num = len(self.pipe.unet.up_blocks)
         for block_i, block in enumerate(self.pipe.unet.up_blocks):
             layer_num = len(getattr(block, "resnets", []))
             for (layer_i, attention) in enumerate(getattr(block, "attentions", [])):
-                self._wrap_block_forward(attention, "attentions", block_num - block_i - 1, layer_num - layer_i - 1, blocktype ="up")
+                self.wrap_block_forward(attention, "attentions", block_num - block_i - 1, layer_num - layer_i - 1, blocktype ="up")
             for (layer_i, resnet) in enumerate(getattr(block, "resnets", [])):
-                self._wrap_block_forward(resnet, "resnet", block_num - block_i - 1, layer_num - layer_i - 1, blocktype ="up")
+                self.wrap_block_forward(resnet, "resnet", block_num - block_i - 1, layer_num - layer_i - 1, blocktype ="up")
             for upsampler in getattr(block, "upsamplers", []) if block.upsamplers else []:
-                self._wrap_block_forward(upsampler, "upsampler", block_num - block_i - 1, 0, blocktype ="up")
-            self._wrap_block_forward(block, "block", block_num - block_i - 1, 0, blocktype ="up")
+                self.wrap_block_forward(upsampler, "upsampler", block_num - block_i - 1, 0, blocktype ="up")
+            self.wrap_block_forward(block, "block", block_num - block_i - 1, 0, blocktype ="up")
 
-    def _unwrap_modules(self):
+    def unwrap_modules(self):
         # 1. unet forward
         self.pipe.unet.forward = self.function_dict['unet_forward']
         # 2. downblock forward
@@ -119,7 +120,7 @@ class DeepCacheSDHelper(object):
                 upsampler.forward = self.function_dict[("up", "upsampler", block_num - block_i - 1, 0)]
             block.forward = self.function_dict[("up", "block", block_num - block_i - 1, 0)]
 
-    def _reset_states(self):
+    def reset_states(self):
         self.cur_timestep = 0
         self.function_dict = {}
         self.cached_output = {}
